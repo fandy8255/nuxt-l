d3c455287debc979cd2be2520a886ac73208ddea
Your Name <you@example.com>
2025-03-25 17:54:09 -0500

    update

--- server/api/send-e.ts
+++ server/api/send-e.ts
@@ -26,18 +26,27 @@ import sgMail from '@sendgrid/mail'
 
 export default defineEventHandler(async (event) => {
     sgMail.setApiKey(process.env.SMTP_PASSWORD!)
+    const runtimeConfig = useRuntimeConfig();
+    const mail = runtimeConfig.mail;
 
     const body = await readBody(event)
 
     if (!body.name || !body.email || !body.subject) {
         throw createError({ statusCode: 400, message: 'Missing required fields' });
-      }
+    }
+
+    const message = `
+    nombre: ${body.name}
+    correo: ${body.email}
+    titulo: ${body.subject}
+    mensaje: ${body.message}
+    `
 
     const msg = {
-        to: 'latinp4453@proton.me', // recipient email
+        to: mail, // recipient email
         from: 'support@latinpanty.com',
         subject: body.subject,
-        text: body.message,
+        text: message,
     }
 
     try {
